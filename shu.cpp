a99f7a461b5ae697f282ec55ff6230c3168a8419
N1ckn1ght <50321432+N1ckn1ght@users.noreply.github.com>
2019-05-06 17:42:19 +0800

    Sources uploaded.
    shu.cpp -> encryptor
    deshu.cpp -> decryptor

---
+++ shu.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <ctype.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int count(char *input) {
+
+	ifstream in(input, ios::in | ios::binary);
+	int count = 0;
+	char check;
+	while ((check = in.get()) != EOF) {
+		count++;
+	}
+	in.close();
+	return count;
+
+}
+
+void encode(char *input, char *output, int len) {
+
+	int ch;
+	int buffer;
+	ifstream in(input, ios::in | ios::binary);
+	ofstream out(output, ios::out | ios::binary);
+	len = len / 8;
+	for (int i = 0; i < (len * 8); i++) {
+		if ((i % 8) == 0) {
+			buffer = in.get();
+			continue;
+		}
+		ch = in.get();
+		ch = ch | ((buffer << (i % 8)) & (1 << 7));
+		if (!in.eof()) {
+			out.put((char)ch);
+		}
+	}
+	in.close();
+	out.close();
+}
+
+int main(int argc, char *argv[]) {
+	int len;
+	len = count(argv[1]);
+	encode(argv[1],argv[2],len);
+	return 0;
+}
